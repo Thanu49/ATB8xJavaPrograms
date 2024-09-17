8db0d1966d2b903af02bca822c6da0fbc66a67ab
Thanu49 <thanushreeag01@gmail.com>
2024-09-17 08:00:10 +0530

    Add files via upload

---
+++ ex_16092024/Lab030_assignment_operator.java
@@ -0,0 +1,12 @@
+package sept.ex_16092024;
+
+import java.sql.SQLOutput;
+
+public class Lab030_assignment_operator {
+    public static void main(String[] args) {
+        int age = 70;
+        //"=" is an assignment operator
+        //assign value to the variable
+        System.out.println(age);
+    }
+}
